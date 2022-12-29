c37a0c980fed977afc58767aa940e7923ccdea22
Brad M. Harris <51719793+HarrisMBrad@users.noreply.github.com>
2022-12-29 21:06:03 +0000

    Initial commit

---
+++ monday/day.js
@@ -0,0 +1,21 @@
+// Start of the Cycle or Loop of Days of the week. 
+// Previouse day or libraries import and export to and for the next week day is the ideas for now. 
+import done from './friday/day';
+
+today = Monday;
+onDays(today, done);
+issues();
+QAneeds();
+meetings = findOut(issues, QAneeds);
+findOut(global(issues), domestic(QAneeds));
+
+console.log(QAneeds, issues);
+
+/**
+ * Start working on Issues assignment discussed from Meetings/ huddles.
+ * > Flow TODO: Continue QA Testing Needs...
+ * > Flow TODO> Curation of Jira backlogs while QA Testing Continues...
+ * 
+ */
+
+export default Monday; 
